38da3e20ad7f82ecda729bd613501f0d690c8709
flaviassantos <32193238+flaviassantos@users.noreply.github.com>
2023-05-03 16:44:42 +0200

    add deploy logic into Jenkinsfile

--- script.groovy
+++ script.groovy
@@ -1,5 +1,8 @@
 def deployApp() {
-    echo 'deploying the application...'
+    sshagent(['ec2-server-key']) {
+        def dockerCmd = "docker run -p 3080:3080 -d flaviassantos/react-nodejs-app:2.0"
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@3.64.127.118 ${dockerCmd}"
+}
 }
 
 return this
