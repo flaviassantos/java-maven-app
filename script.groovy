73218d8d8f7c9d2f6383a6316692a7cb204df09e
flaviassantos <32193238+flaviassantos@users.noreply.github.com>
2023-05-04 04:12:58 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ def deployApp() {
     sshagent(['ec2-server-key']) {
         echo 'deploying docker image to EC2...'
         
-        def dockerComposeCmd = "docker compose -f docker-compose.yaml up --detach"
+        def dockerComposeCmd = "docker-compose -f docker-compose.yaml up --detach"
         sh "scp docker-compose.yaml ec2-user@3.64.127.118:/home/ec2-user"
         sh "ssh -o StrictHostKeyChecking=no ec2-user@3.64.127.118 ${dockerComposeCmd}"
 }
