9df95ee46163989b07f6860559cb3b09e68045ea
flaviassantos <ssantosflavia@hotmail.com>
2023-05-04 04:50:27 +0200

    improvement: extract to Shell Script

--- script.groovy
+++ script.groovy
@@ -1,11 +1,13 @@
 def deployApp() {
-    sshagent(['ec2-server-key']) {
-        echo 'deploying docker image to EC2...'
-        
-        def dockerComposeCmd = "docker-compose -f docker-compose.yaml up --detach"
-        sh "scp docker-compose.yaml ec2-user@3.64.127.118:/home/ec2-user"
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@3.64.127.118 ${dockerComposeCmd}"
-}
-}
+    echo 'deploying docker image to EC2...'
 
+    def shellCmd = "bash ./server-cmds.sh ${IMAGE_NAME}"
+    def ec2Instance = "ec2-user@3.64.127.118"
+
+    sshagent(['ec2-server-key']) {
+        sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user"
+        sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ec2-user"
+        sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} ${shellCmd}"
+    }
+}
 return this
