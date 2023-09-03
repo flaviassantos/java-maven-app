3ce66d17fc88226d54cd465d6253e3d57c6ee465
flaviassantos <32193238+flaviassantos@users.noreply.github.com>
2023-09-03 20:26:41 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -13,14 +13,14 @@ return this
 def deployApp() {
     echo 'deploying docker image to EC2...'
 
-    def shellCmd = "bash ./server-cmds.sh ${IMAGE_NAME}"
-    def ec2Instance = "ec2-user@3.64.127.118"
+    //def shellCmd = "bash ./server-cmds.sh ${IMAGE_NAME}"
+    //def ec2Instance = "ec2-user@3.64.127.118"
 
-    sshagent(['ec2-server-key']) {
-        sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user"
-        sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ec2-user"
-        sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} ${shellCmd}"
-    }
+    //sshagent(['ec2-server-key']) {
+    //    sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user"
+    //    sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ec2-user"
+    //   sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} ${shellCmd}"
+    //}
 }
 return this
 
